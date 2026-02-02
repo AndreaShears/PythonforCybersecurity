577fe19a6de32c864a244fc88b53b61719cb7ce0
edgoad <shears.andrea@student.greenriver.edu>
2026-02-01 17:14:48 -0800

    Hellon World 3

--- start/CH02/HelloWorld3.py
+++ start/CH02/HelloWorld3.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 # A more complex "Hello World" script in python with Inputs
-# Created 
+# Created by Andrea SHears 2/1/2026
+
+#Ask use for their name
+user_name = input("What is your name? ")
+                
+ #Print hello and their name
+print("Hello {0}".format(user_name)) 
+print(f"hello {user_name}")  
+print("Hello" + user_name)
+print("Hello", user_name)
+message="Hello" + user_name
+print(message)

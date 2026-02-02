5d00237e7726b73133baf04c04b5665bfcaf892c
edgoad <shears.andrea@student.greenriver.edu>
2026-02-01 16:48:28 -0800

    started hello world

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
-# Created 
+# Created by Andrea on 2/1/2026
+
+print("Hello World")
--- start/CH02/HelloWorld2.py
+++ start/CH02/HelloWorld2.py
@@ -2,6 +2,8 @@
 # A simple "Hello World" script in python with Inputs
 # Created 
 
-# Suggestion, build out 1 line at a time
-# Once multiple print statemetns exist, put a breakpoint at first print line
-# Then walk through as an example of "debugging"
+#Ask use for their name
+user_name = input("What is your name? ")
+                
+ #Print hello and their name
+print("Hello {0}".format(user_name))           
